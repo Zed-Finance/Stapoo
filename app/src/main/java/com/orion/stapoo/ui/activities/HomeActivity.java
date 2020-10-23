1c3ac4630b6de3d39ccdfa6efc9d4d1665351ddc
Devansh Mehta <iamdev5151@gmail.com>
2020-10-23 22:22:34 +0530

    Added Logout

--- app/src/main/java/com/orion/stapoo/ui/activities/HomeActivity.java
+++ app/src/main/java/com/orion/stapoo/ui/activities/HomeActivity.java
@@ -6,9 +6,11 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.firebase.database.snapshot.LeafNode;
 import com.orion.stapoo.R;
+import com.orion.stapoo.utils.PrefManager;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -17,12 +19,28 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        final PrefManager prefManager = new PrefManager(this);
+
         CardView cardViewLearn = findViewById(R.id.card_view_learn);
 
         cardViewLearn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), LearnActivity.class));
+                finish();
+            }
+        });
+
+        ImageView imgLogout = findViewById(R.id.logout);
+        imgLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                prefManager.setIsLoggedIn(false);
+                prefManager.setIsAvatarChosen(false);
+                prefManager.setUsername(null);
+                prefManager.setAvatar(0);
+
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 finish();
             }
         });
--- app/src/main/java/com/orion/stapoo/ui/activities/VideoActivity.java
+++ app/src/main/java/com/orion/stapoo/ui/activities/VideoActivity.java
@@ -1,16 +1,71 @@
 package com.orion.stapoo.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.orion.stapoo.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class VideoActivity extends AppCompatActivity {
+
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        fetchAndPlayVideo(getIntent().getStringExtra("subject"));
+
+        Button gotoQuiz = findViewById(R.id.btn_quiz);
+        gotoQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+                intent.putExtra("subject", getIntent().getStringExtra("subject"));
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
+    private void fetchAndPlayVideo(String subject) {
+        FirebaseDatabase.getInstance().getReference().child("subjects").child(subject).child("video").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String videoId = snapshot.getValue(String.class);
+                playVideo(videoId);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void playVideo(final String videoId) {
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+    }
+
 }
