911740e0b681fecc38e2a5dd353f8966109b3620
Devansh Mehta <iamdev5151@gmail.com>
2020-10-24 16:22:20 +0530

    Added Progress Lock

--- app/src/main/java/com/orion/stapoo/ui/activities/JourneyActivity.java
+++ app/src/main/java/com/orion/stapoo/ui/activities/JourneyActivity.java
@@ -1,18 +1,35 @@
 package com.orion.stapoo.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.orion.stapoo.R;
+import com.orion.stapoo.utils.PrefManager;
 
-public class JourneyActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class JourneyActivity extends AppCompatActivity implements View.OnClickListener {
 
     String subject;
+    Integer day;
+    String username;
+    List<String> proofListKeys;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,27 +37,74 @@ public class JourneyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_journey);
 
         subject = getIntent().getStringExtra("subject");
+        username = new PrefManager(this).getUsername();
+        proofListKeys = new ArrayList<>();
 
         Button levelOne = findViewById(R.id.level_1);
         Button levelTwo = findViewById(R.id.level_2);
+        Button levelThree = findViewById(R.id.level_3);
+        Button levelFour = findViewById(R.id.level_4);
 
-        levelOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        levelOne.setOnClickListener(this);
+
+        levelTwo.setOnClickListener(this);
+
+        levelThree.setOnClickListener(this);
+
+        levelFour.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.level_1:
+                day = 0;
                 Intent intent = new Intent(getApplicationContext(), TaskActivity.class);
                 intent.putExtra("subject", subject);
-                intent.putExtra("day", "0");
+                intent.putExtra("day", day.toString());
                 startActivity(intent);
+                break;
+            case R.id.level_2:
+                day = 1;
+                checkPrevProgressAndNavigate(day.toString(), String.valueOf(day - 1));
+                break;
+            case R.id.level_3:
+                day = 2;
+                checkPrevProgressAndNavigate(day.toString(), String.valueOf(day - 1));
+                break;
+            case R.id.level_4:
+                day = 3;
+                checkPrevProgressAndNavigate(day.toString(), String.valueOf(day - 1));
+                break;
+
+        }
+    }
+
+    private void checkPrevProgressAndNavigate(final String currentDay, final String prevDay) {
+        FirebaseDatabase.getInstance().getReference().child("subjects").child(subject).child(prevDay).child("proofList").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                proofListKeys.clear();
+                if (snapshot.exists()) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        proofListKeys.add(dataSnapshot.getKey());
+                    }
+                    if (proofListKeys.contains(username)) {
+                        Intent intent = new Intent(getApplicationContext(), TaskActivity.class);
+                        intent.putExtra("subject", subject);
+                        intent.putExtra("day", currentDay);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Please complete previous day's tasks", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please complete previous day's tasks", Toast.LENGTH_LONG).show();
+                }
             }
-        });
 
-        levelTwo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), TaskActivity.class);
-                intent.putExtra("subject", subject);
-                intent.putExtra("day", "1");
-                startActivity(intent);
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
